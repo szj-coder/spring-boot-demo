a61850b4b7faa64ac554f822bf7a14ffc4a5070f
shenzhengjie <zhengjie.shen@iquantex.com>
2019-04-23 10:35:29 +0800

    kafka代码完善

--- src/main/java/com/test/demo/kafkaTest/KafkaConsumerNew.java
+++ src/main/java/com/test/demo/kafkaTest/KafkaConsumerNew.java
@@ -1,41 +1,53 @@
 package com.test.demo.kafkaTest;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
 public class KafkaConsumerNew {
 
 
-
     private Consumer<String, String> consumer;
 
-    private static String group = "group-1";
+    private static String group = "szj-test";
 
-    private static String TOPIC = "topic_ss";
+    private static String TOPIC = "event_topic";
 
     private KafkaConsumerNew() {
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "106.14.191.112:9092");
-//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.95.177.192:9092,10.95.97.175:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.16.18.213:9093,10.16.18.213:9094,10.16.18.214:9093,10.16.18.214:9094");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, group);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true"); // 自动commit
-        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000"); // 自动commit的间隔
+        // 自动commit
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        // 自动commit的间隔
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        consumer = new KafkaConsumer<String, String>(props);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumer = new KafkaConsumer<>(props);
+
     }
 
     private void consume() {
-        consumer.subscribe(Arrays.asList(TOPIC)); // 可消费多个topic,组成一个list
+        // 可消费多个topic,组成一个list
+        consumer.subscribe(Collections.singletonList(TOPIC));
+//        TopicPartition partition = new TopicPartition(TOPIC, 0);
+//
+//        consumer.assign(Collections.singletonList(partition));
+//        consumer.seek(partition, consumer.position(partition));
 
+//        TopicPartition topicPartition = new TopicPartition(TOPIC, 0);
+//        consumer.seek(topicPartition, 0);
         while (true) {
+
             ConsumerRecords<String, String> records = consumer.poll(2000);
             for (ConsumerRecord<String, String> record : records) {
-                System.out.printf("offset = %d, key = %s, value = %s \n", record.offset(), record.key(), record.value());
+                System.out.printf("======： offset = %d, key = %s, value = %s \n", record.offset(), record.key(), record.value());
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
@@ -46,6 +58,9 @@ public class KafkaConsumerNew {
     }
 
     public static void main(String[] args) {
+        System.out.println(StringDeserializer.class.getName());
+        System.out.println(JSONObject.toJSONString(Collections.singletonList(TOPIC)));
+        System.out.println(JSONObject.toJSONString(Arrays.asList(TOPIC)));
         new KafkaConsumerNew().consume();
     }
 
--- src/main/java/com/test/demo/kafkaTest/KafkaProducerNew.java
+++ src/main/java/com/test/demo/kafkaTest/KafkaProducerNew.java
@@ -10,30 +10,45 @@ import java.util.Properties;
 public class KafkaProducerNew {
 
 
-
     private final KafkaProducer<String, String> producer;
 
-    public final static String TOPIC = "topic_ss";
+    public final static String TOPIC = "event_topic";
 
     private KafkaProducerNew() {
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "106.14.191.112:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.16.18.213:9093,10.16.18.213:9094,10.16.18.214:9093,10.16.18.214:9094");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        // 重试
+        props.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
+        // 最大未确认请求数
+        props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 1);
+
+        // 延迟
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 5);
+        // 批量发送大小（以字节为单位）
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 0);
+
+        // 缓存区大小(字节)
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 1000);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        producer = new KafkaProducer<String, String>(props);
+        producer = new KafkaProducer<>(props);
     }
 
     public void produce() {
         int messageNo = 1;
         final int COUNT = 10;
 
-        while(messageNo < COUNT) {
+        while (messageNo < COUNT) {
             String key = String.valueOf(messageNo);
             String data = String.format("hello KafkaProducer message %s", key);
 
             try {
-                producer.send(new ProducerRecord<String, String>(TOPIC, data));
+                // get() 可以同步发送，不调用get 为异步发送
+                producer.send(new ProducerRecord<>(TOPIC, key, data)).get();
+                System.out.println("send: " + data);
+                Thread.sleep(1000);
             } catch (Exception e) {
                 e.printStackTrace();
             }
